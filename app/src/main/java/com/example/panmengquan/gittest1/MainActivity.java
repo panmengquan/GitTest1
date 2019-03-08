4550694e00a60b0961a2c1950bef472b07d3ba14
panmengquan <1253177019@qq.com>
2019-03-08 13:53:26 +0800

    zero

---
+++ app/src/main/java/com/example/panmengquan/gittest1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.panmengquan.gittest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private String version = "version0";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
